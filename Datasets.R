9162b4c556aa18f93454619fb5cad1b7b2498eb9
unknown <joaovitosborges@outlook.com>
2025-09-25 00:28:25 -0300

    atualizar

---
+++ Datasets.R
@@ -0,0 +1,25 @@
+#operações matematicas
+
+2+2
+10/5
+3^2 
+
+#variaveis
+
+x <- 10
+y <- 20
+
+soma <- x + y 
+
+print(soma)
+
+
+idades <- c(22, 25, 30, 40)
+mean(idades)   # média
+max(idades)    # valor máximo
+min(idades)    # valor mínimo
+
+# Estruturas de dados
+nomes <- c("João", "Maria", "Pedro", "Ana")
+df <- data.frame(Nome = nomes, Idade = idades)
+print(df)
